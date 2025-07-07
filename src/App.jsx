bb48d5a28f1c2ed73113852f57533ca471f5dd6d
Miranda Kachlavashvili <136162510+Miranda-K12@users.noreply.github.com>
2025-07-07 23:46:36 +0400

    Import css file in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import AppRoutes from "./routes/AppRoutes";
 import { BrowserRouter as Router } from "react-router-dom";
-
+import "./styles/variables.css";
 function App() {
   return (
     <Router>
