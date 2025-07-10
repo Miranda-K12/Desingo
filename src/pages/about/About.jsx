24aad0a7a664025e5e9530b37d364737cd3f3d45
tamuna-tamo <102787453+TAMUNA-CHKOIDZE@users.noreply.github.com>
2025-07-10 21:07:31 +0400

    add hero on about

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import HeroSection from "../../components/sharedComponents/heroSection/HeroSection"
 
 function About() {
-  return <div></div>;
+  return <div>
+     {/* HeroSection */}
+      <HeroSection
+        title="About Us"
+        description="Founded in 2010, we are a creative agency that produces lasting results for our clients. We’ve partnered with many startups, corporations, and nonprofits alike to craft designs that make real impact. We’re always looking forward to creating brands, products, and digital experiences that connect with our clients’ audiences."
+        floatingImage="aboutFloating"
+        variant="about"
+      />
+  </div>;
 }
 
 export default About;
