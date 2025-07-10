14acdb7b70c3ac173623cc9400d522fed7a7a2f6
tamuna-tamo <102787453+TAMUNA-CHKOIDZE@users.noreply.github.com>
2025-07-10 21:00:43 +0400

    add hero on home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,17 @@
 import React from "react";
-
+import styles from "./Home.module.css";
+import HeroSection from "../../components/sharedComponents/heroSection/HeroSection";
 function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <HeroSection
+        title="Award-winning custom designs and digital branding solutions"
+        description="With over 10 years in the industry, we are experienced in creating fully responsive websites, app design, and engaging brand experiences. Find out more about our services."
+        buttonText="Learn More"
+        variant="home"
+      />
+    </div>
+  );
 }
 
 export default Home;
