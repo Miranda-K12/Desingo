2f75b52b33d69e278f4ce8de703487b1526bcdf2
tamuna-tamo <102787453+TAMUNA-CHKOIDZE@users.noreply.github.com>
2025-07-10 21:20:46 +0400

    add hero on Graphic Design page

---
+++ src/pages/graphicDesign/GraphicDesign.jsx
@@ -0,0 +1,16 @@
+import HeroSection from "../../components/sharedComponents/heroSection/HeroSection";
+
+function GraphicDesign() {
+  return (
+    <div>
+      <HeroSection
+        title="Graphic Design"
+        description="We deliver eye-catching branding materials that are 
+tailored to meet your business objectives."
+        variant="graphic"
+      />
+    </div>
+  );
+}
+
+export default GraphicDesign;
