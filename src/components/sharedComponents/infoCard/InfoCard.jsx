1fd178d0a207bade697ba32a76388bc2853deaef
Miranda Kachlavashvili <136162510+Miranda-K12@users.noreply.github.com>
2025-07-10 21:46:00 +0400

    Update InfoCard

---
+++ src/components/sharedComponents/infoCard/InfoCard.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import styles from "./InfoCard.module.css";
+import arrow from "../../../assets/shared/desktop/icon-right-arrow.svg";
+
+
+function InfoCard({ title, link, backgroundImage, className}) {
+  return (
+    <Link to={link} className={`${styles.blackcard} ${className}`} style={{ backgroundImage: url(${backgroundImage}) }}>
+      <div className={styles.overlay}>
+        <h2>{title}</h2>
+         <p>
+          View Projects 
+          <img src={arrow} alt="arrow" className={styles.arrow}/>
+        </p>
+        
+      </div>
+    </Link>
+  );
+}
+
+export default InfoCard;
