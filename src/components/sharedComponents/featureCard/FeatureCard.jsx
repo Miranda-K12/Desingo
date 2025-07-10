11d724de87ecaac34ac3fc4691dc2d228f317d38
Miranda Kachlavashvili <136162510+Miranda-K12@users.noreply.github.com>
2025-07-10 21:48:54 +0400

    Update Feature Card

---
+++ src/components/sharedComponents/featureCard/FeatureCard.jsx
@@ -0,0 +1,16 @@
+import react from "react";
+import styles from "./FeatureCard.module.css";
+
+function FeatureCard({ title, description, image }) {
+  return (
+    <div className={styles.cards}>
+      <img src={image} alt={title} />
+      <div className={styles.info}>
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default FeatureCard;
