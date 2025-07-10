b8220497770c2e10e28467c0524299d49c59cec0
tamuna-tamo <102787453+TAMUNA-CHKOIDZE@users.noreply.github.com>
2025-07-10 21:29:38 +0400

    add hero component on web design page

---
+++ src/pages/webDesing/WebDesign.jsx
@@ -0,0 +1,16 @@
+import HeroSection from "../../components/sharedComponents/heroSection/HeroSection";
+
+function WebDesign() {
+  return (
+    <div>
+      <HeroSection
+        title="Web Design"
+        description="We build websites that serve as powerful marketing tools 
+and bring memorable brand experiences."
+        variant="web"
+      />
+    </div>
+  );
+}
+
+export default WebDesign;
