1de7786f3b03c5a0705c120235121c41cd5c6eca
tamuna-tamo <102787453+TAMUNA-CHKOIDZE@users.noreply.github.com>
2025-07-10 21:14:03 +0400

    add hero on appDesign

--- src/pages/appDesign/AppDesign.jsx
+++ src/pages/appDesign/AppDesign.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import HeroSection from "../../components/sharedComponents/heroSection/HeroSection";
 
 function AppDesign() {
-  return <div></div>;
+  return (
+    <div>
+      <HeroSection
+        title="App Design"
+        description="Our mobile designs bring intuitive digital solutions
+ to your customers right at their fingertips."
+        variant="app"
+      />
+    </div>
+  );
 }
 
 export default AppDesign;
