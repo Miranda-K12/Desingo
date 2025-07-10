a400f3f66dbb176849e1df4640ac0f1451f83006
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2025-07-10 23:41:41 +0400

    fix horizontal scroll

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -89,7 +89,7 @@ function Footer() {
         flexDirection: "column",
         justifyContent: "space-between",
         gap: 2,
-        width: "100vw",
+        width: "100%",
       }}
     >
       {isMobile ? (
