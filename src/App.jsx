30e2c408602b288a51d5828bccfa24e41ba44672
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2025-07-12 20:40:23 +0400

    add conditional rendering for CallToAction to be excluded on contact form page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import "./App.css";
 import AppRoutes from "./routes/AppRoutes";
-import { BrowserRouter as Router } from "react-router-dom";
 import "./styles/variables.css";
 import Header from "./components/header/Header";
 import CallToAction from "./components/sharedComponents/callToAction/CallToAction";
 import Footer from "./components/footer/Footer";
+import { useLocation } from "react-router-dom";
+
 function App() {
+  const location = useLocation();
+  const isContactPage = location.pathname === "/contact";
+
   return (
-    <Router>
-      <Header />
-      <main>
+    <>
+    <Header />
+      <main className="main-content-container">
         <AppRoutes />
       </main>
-      <CallToAction />
+      {!isContactPage && <CallToAction />}
       <Footer />
-    </Router>
+    </>
   );
 }
 
