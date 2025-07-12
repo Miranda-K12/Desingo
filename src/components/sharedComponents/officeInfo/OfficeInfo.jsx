b44054ebb7b9837360fdfca304083ae3d61f7717
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2025-07-12 20:50:05 +0400

    add redirection to locations page on OfficeInfo component button

--- src/components/sharedComponents/officeInfo/OfficeInfo.jsx
+++ src/components/sharedComponents/officeInfo/OfficeInfo.jsx
@@ -1,11 +1,15 @@
 import styles from "./OfficeInfo.module.css";
 import Button from "../button/Button";
+import { useNavigate } from "react-router-dom";
+
 function CountryCard({ imageSrc, title, buttonText }) {
+  const navigate = useNavigate();
+
   return (
     <div className={styles.cardWrapper}>
       <img src={imageSrc} alt={title} className={styles.CountryImage} />
       <h3 className={styles.cardTitle}>{title}</h3>
-      <Button>{buttonText}</Button>
+      <Button onClick={() => navigate("/locations")}>{buttonText}</Button>
     </div>
   );
 }
